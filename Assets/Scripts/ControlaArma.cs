1af4abce7fb50b27d7a8487e435ba8e158e97fd3
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-05-04 16:47:38 -0300

    Programacao dos tiros

---
+++ Assets/Scripts/ControlaArma.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaArma : MonoBehaviour
+{
+    public GameObject WeaponBarrel;
+    public GameObject Bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Bullet, WeaponBarrel.transform.position, WeaponBarrel.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/ControlaBala.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaBala : MonoBehaviour
+{
+    public int velocidade = 30;
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.forward * velocidade * Time.deltaTime);
+    }
+}
