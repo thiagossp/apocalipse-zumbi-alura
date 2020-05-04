42f56ba7bcc9e0c0840689b5eac09f6a8acc2121
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-05-04 15:47:24 -0300

    Programacao dos movimentos dos inimigos

---
+++ Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour
+{
+    public GameObject Jogador;
+    public int velocidade = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
+
+        if (distancia > 2.5)
+        {
+            Vector3 direcao = Jogador.transform.position - transform.position;
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * velocidade * Time.deltaTime);
+
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
+
+    }
+}
