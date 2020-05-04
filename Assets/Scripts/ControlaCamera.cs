aa9c374361093a0856a94f3c158aa7248f38734e
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-05-04 14:48:07 -0300

    Inicio do projeto, criacao do cenario, criacao do personagem e programacao dos movimentos do personagem

---
+++ Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ControlaCamera : MonoBehaviour
+{
+    public GameObject Jogador;
+    Vector3 distanciaCompensar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanciaCompensar = transform.position - Jogador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Jogador.transform.position + distanciaCompensar;
+    }
+}
---
+++ Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+    public int velocidade = 10;
+    private Vector3 direcao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(eixoX, 0, eixoZ);
+        GetComponent<Rigidbody>().position = GetComponent<Rigidbody>().position + (direcao * velocidade * Time.deltaTime);
+
+        if (direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("isRunning", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isRunning", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao * velocidade * Time.deltaTime));
+    }
+}
